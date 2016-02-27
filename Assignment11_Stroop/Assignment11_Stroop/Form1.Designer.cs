21c6ceeeacb4c1b41decf8d39b2686142e50e514
Jacob Stroop <israel941@gmail.com>
2016-02-26 21:07:56 -0500

    Adds writing to file.

--- Assignment11_Stroop/Assignment11_Stroop/Form1.Designer.cs
+++ Assignment11_Stroop/Assignment11_Stroop/Form1.Designer.cs
@@ -30,27 +30,27 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.entryTab = new System.Windows.Forms.TabPage();
-            this.readTab = new System.Windows.Forms.TabPage();
-            this.firstNameLbl = new System.Windows.Forms.Label();
-            this.lastNameLbl = new System.Windows.Forms.Label();
-            this.phoneNumberLbl = new System.Windows.Forms.Label();
-            this.birthMonthLbl = new System.Windows.Forms.Label();
-            this.birthDayLbl = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.enterBtn = new System.Windows.Forms.Button();
             this.exitBtn = new System.Windows.Forms.Button();
-            this.reminderTab = new System.Windows.Forms.TabPage();
-            this.readListBox = new System.Windows.Forms.ListBox();
-            this.readBtn = new System.Windows.Forms.Button();
+            this.enterBtn = new System.Windows.Forms.Button();
+            this.phoneNumTxt = new System.Windows.Forms.MaskedTextBox();
+            this.birthDayTxt = new System.Windows.Forms.TextBox();
+            this.birthMonthTxt = new System.Windows.Forms.TextBox();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.firstNameTxt = new System.Windows.Forms.TextBox();
+            this.birthDayLbl = new System.Windows.Forms.Label();
+            this.birthMonthLbl = new System.Windows.Forms.Label();
+            this.phoneNumberLbl = new System.Windows.Forms.Label();
+            this.lastNameLbl = new System.Windows.Forms.Label();
+            this.firstNameLbl = new System.Windows.Forms.Label();
+            this.readTab = new System.Windows.Forms.TabPage();
             this.exitBtn2 = new System.Windows.Forms.Button();
-            this.reminderListBox = new System.Windows.Forms.ListBox();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.readListBox = new System.Windows.Forms.ListBox();
+            this.reminderTab = new System.Windows.Forms.TabPage();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.exitBtn3 = new System.Windows.Forms.Button();
             this.reminderBtn = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.reminderListBox = new System.Windows.Forms.ListBox();
             this.tabControl1.SuspendLayout();
             this.entryTab.SuspendLayout();
             this.readTab.SuspendLayout();
@@ -72,11 +72,11 @@
             // 
             this.entryTab.Controls.Add(this.exitBtn);
             this.entryTab.Controls.Add(this.enterBtn);
-            this.entryTab.Controls.Add(this.maskedTextBox1);
-            this.entryTab.Controls.Add(this.textBox4);
-            this.entryTab.Controls.Add(this.textBox3);
-            this.entryTab.Controls.Add(this.textBox2);
-            this.entryTab.Controls.Add(this.textBox1);
+            this.entryTab.Controls.Add(this.phoneNumTxt);
+            this.entryTab.Controls.Add(this.birthDayTxt);
+            this.entryTab.Controls.Add(this.birthMonthTxt);
+            this.entryTab.Controls.Add(this.lastNameTxt);
+            this.entryTab.Controls.Add(this.firstNameTxt);
             this.entryTab.Controls.Add(this.birthDayLbl);
             this.entryTab.Controls.Add(this.birthMonthLbl);
             this.entryTab.Controls.Add(this.phoneNumberLbl);
@@ -89,6 +89,105 @@
             this.entryTab.TabIndex = 0;
             this.entryTab.Text = "Entry";
             this.entryTab.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(196, 203);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(100, 23);
+            this.exitBtn.TabIndex = 11;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // enterBtn
+            // 
+            this.enterBtn.Location = new System.Drawing.Point(24, 203);
+            this.enterBtn.Name = "enterBtn";
+            this.enterBtn.Size = new System.Drawing.Size(100, 23);
+            this.enterBtn.TabIndex = 10;
+            this.enterBtn.Text = "Enter Friend";
+            this.enterBtn.UseVisualStyleBackColor = true;
+            this.enterBtn.Click += new System.EventHandler(this.enterBtn_Click);
+            // 
+            // phoneNumTxt
+            // 
+            this.phoneNumTxt.Location = new System.Drawing.Point(196, 88);
+            this.phoneNumTxt.Name = "phoneNumTxt";
+            this.phoneNumTxt.Size = new System.Drawing.Size(100, 20);
+            this.phoneNumTxt.TabIndex = 9;
+            // 
+            // birthDayTxt
+            // 
+            this.birthDayTxt.Location = new System.Drawing.Point(196, 150);
+            this.birthDayTxt.Name = "birthDayTxt";
+            this.birthDayTxt.Size = new System.Drawing.Size(100, 20);
+            this.birthDayTxt.TabIndex = 8;
+            // 
+            // birthMonthTxt
+            // 
+            this.birthMonthTxt.Location = new System.Drawing.Point(196, 120);
+            this.birthMonthTxt.Name = "birthMonthTxt";
+            this.birthMonthTxt.Size = new System.Drawing.Size(100, 20);
+            this.birthMonthTxt.TabIndex = 7;
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(196, 58);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTxt.TabIndex = 6;
+            // 
+            // firstNameTxt
+            // 
+            this.firstNameTxt.Location = new System.Drawing.Point(196, 29);
+            this.firstNameTxt.Name = "firstNameTxt";
+            this.firstNameTxt.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTxt.TabIndex = 5;
+            // 
+            // birthDayLbl
+            // 
+            this.birthDayLbl.AutoSize = true;
+            this.birthDayLbl.Location = new System.Drawing.Point(21, 150);
+            this.birthDayLbl.Name = "birthDayLbl";
+            this.birthDayLbl.Size = new System.Drawing.Size(50, 13);
+            this.birthDayLbl.TabIndex = 4;
+            this.birthDayLbl.Text = "Birth Day";
+            // 
+            // birthMonthLbl
+            // 
+            this.birthMonthLbl.AutoSize = true;
+            this.birthMonthLbl.Location = new System.Drawing.Point(21, 120);
+            this.birthMonthLbl.Name = "birthMonthLbl";
+            this.birthMonthLbl.Size = new System.Drawing.Size(61, 13);
+            this.birthMonthLbl.TabIndex = 3;
+            this.birthMonthLbl.Text = "Birth Month";
+            // 
+            // phoneNumberLbl
+            // 
+            this.phoneNumberLbl.AutoSize = true;
+            this.phoneNumberLbl.Location = new System.Drawing.Point(21, 88);
+            this.phoneNumberLbl.Name = "phoneNumberLbl";
+            this.phoneNumberLbl.Size = new System.Drawing.Size(78, 13);
+            this.phoneNumberLbl.TabIndex = 2;
+            this.phoneNumberLbl.Text = "Phone Number";
+            // 
+            // lastNameLbl
+            // 
+            this.lastNameLbl.AutoSize = true;
+            this.lastNameLbl.Location = new System.Drawing.Point(21, 58);
+            this.lastNameLbl.Name = "lastNameLbl";
+            this.lastNameLbl.Size = new System.Drawing.Size(58, 13);
+            this.lastNameLbl.TabIndex = 1;
+            this.lastNameLbl.Text = "Last Name";
+            // 
+            // firstNameLbl
+            // 
+            this.firstNameLbl.AutoSize = true;
+            this.firstNameLbl.Location = new System.Drawing.Point(21, 29);
+            this.firstNameLbl.Name = "firstNameLbl";
+            this.firstNameLbl.Size = new System.Drawing.Size(57, 13);
+            this.firstNameLbl.TabIndex = 0;
+            this.firstNameLbl.Text = "First Name";
             // 
             // readTab
             // 
@@ -103,103 +202,31 @@
             this.readTab.Text = "Read";
             this.readTab.UseVisualStyleBackColor = true;
             // 
-            // firstNameLbl
+            // exitBtn2
             // 
-            this.firstNameLbl.AutoSize = true;
-            this.firstNameLbl.Location = new System.Drawing.Point(21, 29);
-            this.firstNameLbl.Name = "firstNameLbl";
-            this.firstNameLbl.Size = new System.Drawing.Size(57, 13);
-            this.firstNameLbl.TabIndex = 0;
-            this.firstNameLbl.Text = "First Name";
+            this.exitBtn2.Location = new System.Drawing.Point(194, 235);
+            this.exitBtn2.Name = "exitBtn2";
+            this.exitBtn2.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn2.TabIndex = 2;
+            this.exitBtn2.Text = "Exit";
+            this.exitBtn2.UseVisualStyleBackColor = true;
             // 
-            // lastNameLbl
+            // readBtn
             // 
-            this.lastNameLbl.AutoSize = true;
-            this.lastNameLbl.Location = new System.Drawing.Point(21, 58);
-            this.lastNameLbl.Name = "lastNameLbl";
-            this.lastNameLbl.Size = new System.Drawing.Size(58, 13);
-            this.lastNameLbl.TabIndex = 1;
-            this.lastNameLbl.Text = "Last Name";
+            this.readBtn.Location = new System.Drawing.Point(7, 235);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(75, 23);
+            this.readBtn.TabIndex = 1;
+            this.readBtn.Text = "Read";
+            this.readBtn.UseVisualStyleBackColor = true;
             // 
-            // phoneNumberLbl
+            // readListBox
             // 
-            this.phoneNumberLbl.AutoSize = true;
-            this.phoneNumberLbl.Location = new System.Drawing.Point(21, 88);
-            this.phoneNumberLbl.Name = "phoneNumberLbl";
-            this.phoneNumberLbl.Size = new System.Drawing.Size(78, 13);
-            this.phoneNumberLbl.TabIndex = 2;
-            this.phoneNumberLbl.Text = "Phone Number";
-            // 
-            // birthMonthLbl
-            // 
-            this.birthMonthLbl.AutoSize = true;
-            this.birthMonthLbl.Location = new System.Drawing.Point(21, 120);
-            this.birthMonthLbl.Name = "birthMonthLbl";
-            this.birthMonthLbl.Size = new System.Drawing.Size(61, 13);
-            this.birthMonthLbl.TabIndex = 3;
-            this.birthMonthLbl.Text = "Birth Month";
-            // 
-            // birthDayLbl
-            // 
-            this.birthDayLbl.AutoSize = true;
-            this.birthDayLbl.Location = new System.Drawing.Point(21, 150);
-            this.birthDayLbl.Name = "birthDayLbl";
-            this.birthDayLbl.Size = new System.Drawing.Size(50, 13);
-            this.birthDayLbl.TabIndex = 4;
-            this.birthDayLbl.Text = "Birth Day";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(196, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(196, 58);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(196, 120);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(196, 150);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 8;
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(196, 88);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 9;
-            // 
-            // enterBtn
-            // 
-            this.enterBtn.Location = new System.Drawing.Point(24, 203);
-            this.enterBtn.Name = "enterBtn";
-            this.enterBtn.Size = new System.Drawing.Size(100, 23);
-            this.enterBtn.TabIndex = 10;
-            this.enterBtn.Text = "Enter Friend";
-            this.enterBtn.UseVisualStyleBackColor = true;
-            // 
-            // exitBtn
-            // 
-            this.exitBtn.Location = new System.Drawing.Point(196, 203);
-            this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(100, 23);
-            this.exitBtn.TabIndex = 11;
-            this.exitBtn.Text = "Exit";
-            this.exitBtn.UseVisualStyleBackColor = true;
+            this.readListBox.FormattingEnabled = true;
+            this.readListBox.Location = new System.Drawing.Point(7, 6);
+            this.readListBox.Name = "readListBox";
+            this.readListBox.Size = new System.Drawing.Size(469, 199);
+            this.readListBox.TabIndex = 0;
             // 
             // reminderTab
             // 
@@ -215,39 +242,12 @@
             this.reminderTab.Text = "Reminder";
             this.reminderTab.UseVisualStyleBackColor = true;
             // 
-            // readListBox
+            // textBox5
             // 
-            this.readListBox.FormattingEnabled = true;
-            this.readListBox.Location = new System.Drawing.Point(7, 6);
-            this.readListBox.Name = "readListBox";
-            this.readListBox.Size = new System.Drawing.Size(469, 199);
-            this.readListBox.TabIndex = 0;
-            // 
-            // readBtn
-            // 
-            this.readBtn.Location = new System.Drawing.Point(7, 235);
-            this.readBtn.Name = "readBtn";
-            this.readBtn.Size = new System.Drawing.Size(75, 23);
-            this.readBtn.TabIndex = 1;
-            this.readBtn.Text = "Read";
-            this.readBtn.UseVisualStyleBackColor = true;
-            // 
-            // exitBtn2
-            // 
-            this.exitBtn2.Location = new System.Drawing.Point(194, 235);
-            this.exitBtn2.Name = "exitBtn2";
-            this.exitBtn2.Size = new System.Drawing.Size(75, 23);
-            this.exitBtn2.TabIndex = 2;
-            this.exitBtn2.Text = "Exit";
-            this.exitBtn2.UseVisualStyleBackColor = true;
-            // 
-            // reminderListBox
-            // 
-            this.reminderListBox.FormattingEnabled = true;
-            this.reminderListBox.Location = new System.Drawing.Point(7, 5);
-            this.reminderListBox.Name = "reminderListBox";
-            this.reminderListBox.Size = new System.Drawing.Size(469, 199);
-            this.reminderListBox.TabIndex = 1;
+            this.textBox5.Location = new System.Drawing.Point(32, 231);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
             // 
             // exitBtn3
             // 
@@ -267,12 +267,13 @@
             this.reminderBtn.Text = "Reminder";
             this.reminderBtn.UseVisualStyleBackColor = true;
             // 
-            // textBox5
+            // reminderListBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(32, 231);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 5;
+            this.reminderListBox.FormattingEnabled = true;
+            this.reminderListBox.Location = new System.Drawing.Point(7, 5);
+            this.reminderListBox.Name = "reminderListBox";
+            this.reminderListBox.Size = new System.Drawing.Size(469, 199);
+            this.reminderListBox.TabIndex = 1;
             // 
             // Form1
             // 
@@ -296,11 +297,11 @@
 
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage entryTab;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox phoneNumTxt;
+        private System.Windows.Forms.TextBox birthDayTxt;
+        private System.Windows.Forms.TextBox birthMonthTxt;
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.TextBox firstNameTxt;
         private System.Windows.Forms.Label birthDayLbl;
         private System.Windows.Forms.Label birthMonthLbl;
         private System.Windows.Forms.Label phoneNumberLbl;
--- Assignment11_Stroop/Assignment11_Stroop/Form1.cs
+++ Assignment11_Stroop/Assignment11_Stroop/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,56 @@ namespace Assignment11_Stroop
 {
     public partial class Form1 : Form
     {
+        // declare fields
+        // FileStream readStream;
+        Friend friend;
+        FileStream writeStream;
+        // StreamReader reader;
+        StreamWriter writer;
+
+        // constant(s)
+        const string FILEPATH = "Friends.txt";
+
         public Form1()
         {
             InitializeComponent();
-            Friend friend = new Friend();
+            // instantiate new friend object
+            //this.friend = new Friend();
+            //readStream = new FileStream(FILEPATH, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+            
+
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+            // instantiate new friend
+            friend = new Friend();
+
+            friend.FirstName = firstNameTxt.Text;
+            friend.LastName = lastNameTxt.Text;
+            friend.PhoneNumber = phoneNumTxt.Text;
+            friend.Month = Convert.ToInt32(birthMonthTxt.Text);
+            friend.Day = Convert.ToInt32(birthDayTxt.Text);
+
+            try
+            {
+                writeStream = new FileStream(FILEPATH, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                writer = new StreamWriter(writeStream);
+
+                writer.WriteLine(friend.ToString());
+
+                Console.WriteLine("SUCCESSFUL WRITE");
+            }
+            catch (IOException)
+            {
+            }
+            finally
+            {
+                writer.Close();
+                Console.WriteLine("WRITER CLOSED");
+                writeStream.Close();
+                Console.WriteLine("STREAM CLOSED");
+            }
         }
     }
 }
